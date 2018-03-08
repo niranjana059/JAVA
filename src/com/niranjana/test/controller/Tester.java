40a8ec535161371caf9a5e69bb970b63bd0a5aca
niranjana <niranjana.059@gmail.com>
2018-03-09 00:10:28 +0530

    jx

---
+++ src/com/niranjana/test/controller/Tester.java
@@ -0,0 +1,51 @@
+package com.niranjana.test.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONObject;
+
+/**
+ * Servlet implementation class Tester
+ */
+@WebServlet("/Tester")
+public class Tester extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Tester() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("application/json");
+		PrintWriter out=response.getWriter();
+		JSONObject obj=new JSONObject();
+		obj.put("ind", "India");
+		obj.put("pak", "pakistan");
+		obj.put("aus", "Australia");
+		out.print(obj);
+		out.flush();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
